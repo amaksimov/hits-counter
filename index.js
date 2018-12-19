e0e530905825fa0efe5f519630763dcc12d7ba41
Alex Maximov <pumpupumpu@rambler.ru>
2018-12-19 09:56:39 +0300

    Add ESLint

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 
-var pgp = require('pg-promise')();
+const app = express();
+
+const pgp = require('pg-promise')();
+
 const db = pgp('postgres://postgres:postgres@db:5432/hits-counter_dev');
 
-app.get('/', function (request, response) {
-    db.none('INSERT INTO hits(created_at) VALUES(${timestamp})', {
-        timestamp: new Date()
-    })
-
-    db.one('select count(*) from hits where created_at > $1', [ new Date(new Date() - 60000)], c => +c.count)
-        .then(count => {
-            response.send({pageviews: count});
+app.get('/', (request, response) => {
+    db.none('INSERT INTO hits(created_at) VALUES($1)', [new Date()])
+        .then(() => {
+            db.one('select count(*) from hits where created_at > $1', [new Date(new Date() - 60000)], c => +c.count)
+                .then((count) => {
+                    response.send({ pageviews: count });
+                });
         });
 });
 
-app.listen(1234)
+app.listen(1234);
